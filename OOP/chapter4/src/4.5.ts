3d8e894ad7af3c82afd993b419ebfdc36219308a
ghooz1204 <ghooz1204@naver.com>
2021-12-05 16:14:30 +0900

    # 4.5 / 4.6 주석 제거

--- OOP/chapter4/src/4.5.ts
+++ OOP/chapter4/src/4.5.ts
@@ -1,73 +1,73 @@
-// interface IBicycle{
-//     ride(): void;
-//     brake(): void;
-// }
+interface IBicycle{
+    ride(): void;
+    brake(): void;
+}
 
-// interface ITrip{
-//     prepareTravel(): void;
-// }
+interface ITrip{
+    prepareTravel(): void;
+}
 
-// interface IMechanic{
-//     cleanBicycle(bicycle: IBicycle): void;
-//     pumpTires(bicycle: IBicycle): void;
-//     lubeChain(bicycle: IBicycle): void;
-//     checkBrakes(bicycle: IBicycle): void;
-// }
+interface IMechanic{
+    cleanBicycle(bicycle: IBicycle): void;
+    pumpTires(bicycle: IBicycle): void;
+    lubeChain(bicycle: IBicycle): void;
+    checkBrakes(bicycle: IBicycle): void;
+}
 
-// class Bicycle implements IBicycle{
-//     constructor(private size: number){}
-//     ride(): void {
-//         console.log('ride');
-//     }
-//     brake(): void {
-//         console.log('brake');
-//     }
-// }
+class Bicycle implements IBicycle{
+    constructor(private size: number){}
+    ride(): void {
+        console.log('ride');
+    }
+    brake(): void {
+        console.log('brake');
+    }
+}
 
-// class Bicycle2 implements IBicycle{
-//     constructor(private size: number){}
-//     ride(): void {
-//         console.log('ride2');
-//     }
-//     brake(): void {
-//         console.log('brake2');
-//     }
-// }
+class Bicycle2 implements IBicycle{
+    constructor(private size: number){}
+    ride(): void {
+        console.log('ride2');
+    }
+    brake(): void {
+        console.log('brake2');
+    }
+}
 
-// class Mechanic implements IMechanic{
-//     cleanBicycle(bicycle: IBicycle): void{
-//         console.log(bicycle, 'cleanBicycle');
-//     }
-//     pumpTires(bicycle: IBicycle): void {
-//         console.log(bicycle, 'pumpTires');
-//     }
-//     lubeChain(bicycle: IBicycle): void {
-//         console.log(bicycle, 'lubeChain');
-//     }
-//     checkBrakes(bicycle: IBicycle): void {
-//         console.log(bicycle, 'checkBrakes');
-//     }
-//     // add prepare procedure
-//     something(bicycle: IBicycle): void {
-//         console.log(bicycle, 'something');
-//     }
-// }
+class Mechanic implements IMechanic{
+    cleanBicycle(bicycle: IBicycle): void{
+        console.log(bicycle, 'cleanBicycle');
+    }
+    pumpTires(bicycle: IBicycle): void {
+        console.log(bicycle, 'pumpTires');
+    }
+    lubeChain(bicycle: IBicycle): void {
+        console.log(bicycle, 'lubeChain');
+    }
+    checkBrakes(bicycle: IBicycle): void {
+        console.log(bicycle, 'checkBrakes');
+    }
+    // add prepare procedure
+    something(bicycle: IBicycle): void {
+        console.log(bicycle, 'something');
+    }
+}
 
-// class Trip implements ITrip{
-//     constructor(private bicycles: IBicycle[], private mechanic: IMechanic){}
-//     prepareTravel(): void {
-//         this.bicycles.forEach(bicycle => {
-//             this.mechanic.cleanBicycle(bicycle);
-//             this.mechanic.pumpTires(bicycle);
-//             this.mechanic.lubeChain(bicycle);
-//             this.mechanic.checkBrakes(bicycle);
-//         })
-//     }
-// }
+class Trip implements ITrip{
+    constructor(private bicycles: IBicycle[], private mechanic: IMechanic){}
+    prepareTravel(): void {
+        this.bicycles.forEach(bicycle => {
+            this.mechanic.cleanBicycle(bicycle);
+            this.mechanic.pumpTires(bicycle);
+            this.mechanic.lubeChain(bicycle);
+            this.mechanic.checkBrakes(bicycle);
+        })
+    }
+}
 
 
-// const bicycles = [new Bicycle(10), new Bicycle2(10)];
-// const mechanic = new Mechanic();
-// const trip = new Trip(bicycles, mechanic);
+const bicycles = [new Bicycle(10), new Bicycle2(10)];
+const mechanic = new Mechanic();
+const trip = new Trip(bicycles, mechanic);
 
-// trip.prepareTravel();
+trip.prepareTravel();
--- OOP/chapter4/src/4.6.ts
+++ OOP/chapter4/src/4.6.ts
@@ -1,73 +1,73 @@
-// interface IBicycle{
-//     ride(): void;
-//     brake(): void;
-// }
+interface IBicycle{
+    ride(): void;
+    brake(): void;
+}
 
-// interface ITrip{
-//     prepareTravel(): void;
-// }
+interface ITrip{
+    prepareTravel(): void;
+}
 
-// interface IMechanic{
-//     prepareBicycle(bicycle: IBicycle[]): void;
-// }
+interface IMechanic{
+    prepareBicycle(bicycle: IBicycle[]): void;
+}
 
-// class Bicycle implements IBicycle{
-//     constructor(private size: number){}
-//     ride(): void {
-//         console.log('ride');
-//     }
-//     brake(): void {
-//         console.log('brake');
-//     }
-// }
+class Bicycle implements IBicycle{
+    constructor(private size: number){}
+    ride(): void {
+        console.log('ride');
+    }
+    brake(): void {
+        console.log('brake');
+    }
+}
 
-// class Bicycle2 implements IBicycle{
-//     constructor(private size: number){}
-//     ride(): void {
-//         console.log('ride2');
-//     }
-//     brake(): void {
-//         console.log('brake2');
-//     }
-// }
+class Bicycle2 implements IBicycle{
+    constructor(private size: number){}
+    ride(): void {
+        console.log('ride2');
+    }
+    brake(): void {
+        console.log('brake2');
+    }
+}
 
-// class Mechanic implements IMechanic{
-//     private cleanBicycle(bicycle: IBicycle): void{
-//         console.log(bicycle, 'cleanBicycle');
-//     }
-//     private pumpTires(bicycle: IBicycle): void {
-//         console.log(bicycle, 'pumpTires');
-//     }
-//     private lubeChain(bicycle: IBicycle): void {
-//         console.log(bicycle, 'lubeChain');
-//     }
-//     private checkBrakes(bicycle: IBicycle): void {
-//         console.log(bicycle, 'checkBrakes');
-//     }
-//     private something(bicycle: IBicycle): void {
-//         console.log(bicycle, 'something');
-//     }
-//     prepareBicycle(bicycles: IBicycle[]): void{
-//         bicycles.forEach(bicycle => {
-//             this.cleanBicycle(bicycle);
-//             this.pumpTires(bicycle);
-//             this.lubeChain(bicycle);
-//             this.checkBrakes(bicycle);
-//             this.something(bicycle);
-//         });
-//     }
-// }
+class Mechanic implements IMechanic{
+    private cleanBicycle(bicycle: IBicycle): void{
+        console.log(bicycle, 'cleanBicycle');
+    }
+    private pumpTires(bicycle: IBicycle): void {
+        console.log(bicycle, 'pumpTires');
+    }
+    private lubeChain(bicycle: IBicycle): void {
+        console.log(bicycle, 'lubeChain');
+    }
+    private checkBrakes(bicycle: IBicycle): void {
+        console.log(bicycle, 'checkBrakes');
+    }
+    private something(bicycle: IBicycle): void {
+        console.log(bicycle, 'something');
+    }
+    prepareBicycle(bicycles: IBicycle[]): void{
+        bicycles.forEach(bicycle => {
+            this.cleanBicycle(bicycle);
+            this.pumpTires(bicycle);
+            this.lubeChain(bicycle);
+            this.checkBrakes(bicycle);
+            this.something(bicycle);
+        });
+    }
+}
 
 
-// class Trip implements ITrip{
-//     constructor(private bicycles: IBicycle[], private mechanic: IMechanic){}
-//     prepareTravel(): void {
-//         this.mechanic.prepareBicycle(this.bicycles);
-//     }
-// }
+class Trip implements ITrip{
+    constructor(private bicycles: IBicycle[], private mechanic: IMechanic){}
+    prepareTravel(): void {
+        this.mechanic.prepareBicycle(this.bicycles);
+    }
+}
 
-// const bicycles = [new Bicycle(10), new Bicycle2(10)];
-// const mechanic = new Mechanic();
-// const trip = new Trip(bicycles, mechanic);
+const bicycles = [new Bicycle(10), new Bicycle2(10)];
+const mechanic = new Mechanic();
+const trip = new Trip(bicycles, mechanic);
 
-// trip.prepareTravel();
+trip.prepareTravel();
